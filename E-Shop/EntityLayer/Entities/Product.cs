7364905248e8e00ad6bfe8fb27a131d2d4ca7104
Samet <samethizli224@gmail.com>
2024-03-16 19:11:24 +0300

    Asp.netMvcOnionArchitectureE-Shop
    E-shop site developed using ASP.net MVC onion architecture

---
+++ E-Shop/EntityLayer/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Açıklama")]
+        [StringLength(50, ErrorMessage = "Max 50 karakter olmalıdır.")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Fiyat")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Stok")]
+        public int Stock { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Popüler")]
+        public bool Popular { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Onay")]
+        public bool isApproved { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Resim")]
+        public string Image { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Adet")]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
